9f6bf57fa1072f10139a2681e8e13bf33e43f3b8
John Washburne Jr <johnwashburne@outlook.com>
2023-01-01 23:44:30 -0500

    main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from prediction_engine import DistributionLoader, Predictor
-import random
 
 if __name__ == "__main__":
     p = Predictor(DistributionLoader.load("default"))
-    dice = [random.randint(1, 6) for _ in range(5)]
-    print(dice)
-    input()
-    print(p.optimize_turn(0, 100, 3, dice))
+    print(p.optimize_turn(
+        current_points=0,
+        score_to_beat=7,
+        opponents_left=4,
+        dice=[4, 3, 1, 1, 2]
+    ))
